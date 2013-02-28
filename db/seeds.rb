546833832c3df119f8763cd89826962105f1a6dc
vla <vladimir@acid.cl>
2013-02-27 23:09:07 -0300

    agrego lista de precios a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,21 @@ users = User.create({email:      "katalina.crespo@gmail.com",
 	                 last_name:  "Crespo",
 	                 password: 123456})
 
-services = Service.create([{name: 'manos', value: 4000},
-	                       {name: 'pies', value: 4000},
-	                       {name: 'brazo', value: 3500}])
+services = Service.create([{name: 'manos full',         		value: 7000},
+	                       {name: 'manos express',      		value: 5500},
+	                       {name: 'pies full',          		value: 11900},
+	                       {name: 'promo manos y pies', 		value: 15900},
+	                       {name: 'promo permanente',   		value: 30000},
+	                       {name: 'manos permanente',   		value: 17900},
+	                       {name: 'pies permanente',    		value: 18900},
+	                       {name: 'cambio esmalte',     		value: 2900},
+	                       {name: 'diseno',                     value: 2900},
+	                       {name: 'depilacion zona facial',     value: 3000},
+	                       {name: 'depilacion axila',           value: 3500},
+	                       {name: 'depilacion pierna completa', value: 6000},
+	                       {name: 'brazos', 					value: 4000},
+	                       {name: 'media pierna', 				value: 4500},
+	                       {name: 'pecho', 						value: 6000},
+	                       {name: 'abdomen', 					value: 6000},
+	                       {name: 'rebaje', 					value: 4500}
+	                       ])
